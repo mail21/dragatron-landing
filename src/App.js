3d5c88043612f41bb028c65e69714d61cf5147f7
mail21 <ismailidn1@gmail.com>
2022-06-24 13:49:36 +0700

    firstt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import arrow from './arrow.svg';
+import Card from './Card.jsx';
 import './App.css';
 
 function App() {
+  const mockData = [
+    {
+      title: 'ALL ACCESS MEMBERSHIP',
+      price: '$299/Month',
+      listStatus: ['1', '1', '1', '1', '1', '1'],
+    },
+    {
+      title: 'IN PERSON TRAINING MEMBERSHIP',
+      price: '$199/Month',
+      listStatus: ['1', '1', '1', '1', '0', '0'],
+    },
+    {
+      title: 'Virtual Training',
+      price: '$99/Month',
+      listStatus: ['1', '1', '1', '1', '0', '0'],
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={arrow} className="App-logo" alt="logo" />
+        <h2>Dragatron.</h2>
       </header>
+
+      <main>
+        <div className="description">
+          <h1>
+            SIGN UP AS A <span className="text-orange">PROFESIONAL</span>
+          </h1>
+          <h4>
+            <span className="text-orange">STEP 3/4</span> SELECT TRAINER SUBSCRIPTION
+          </h4>
+          <h5>
+            With 3 Types of memberships,
+            <span className="text-orange">
+              Discover the membership which fits your personal needs
+            </span>
+          </h5>
+        </div>
+
+        <div className="card-column">
+          <Card
+            isPopular={false}
+            price={mockData[0].price}
+            listStatus={mockData[0].listStatus}
+            title={mockData[0].title}
+          />
+          <Card
+            isPopular={true}
+            price={mockData[1].price}
+            listStatus={mockData[1].listStatus}
+            title={mockData[1].title}
+          />
+          <Card
+            isPopular={false}
+            price={mockData[2].price}
+            listStatus={mockData[2].listStatus}
+            title={mockData[2].title}
+          />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,40 @@
+import './Card.css';
+
+function Card({ title, price, listStatus, isPopular }) {
+  const listText = [
+    'Promote yourself & reach thousands of user globaly',
+    'Accept Payments',
+    'Only',
+    'Access to manager customers health profile',
+    'Verivied Profilde',
+    'Access to weekly newsletters',
+  ];
+
+  return (
+    <div className="card">
+      {isPopular ? <div className="popular">POPULAR.</div> : ''}
+      <div className="card__body">
+        <h3 className="text-main">{title}</h3>
+        <h4>{price}</h4>
+        <ul>
+          {listText.map((item, index) => {
+            if (listStatus[index] == 1) {
+              if (index == 2) {
+                return <li>{`${item} ${title}`}</li>;
+              }
+              return <li>{item}</li>;
+            } else {
+              return <li className="greyed">{item}</li>;
+            }
+          })}
+        </ul>
+      </div>
+
+      <div className="button">
+        <div>PROCEED & PAY</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
